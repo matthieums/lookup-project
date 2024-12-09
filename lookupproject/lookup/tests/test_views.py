47ec38fa344f0a366fd71b9a5b46092629441e25
Matthieu <martens.matthieu@hotmail.com>
2024-12-09 12:22:13 +0100

    Add tests to check each views' response

--- lookupproject/lookup/tests/test_views.py
+++ lookupproject/lookup/tests/test_views.py
@@ -6,26 +6,21 @@ class test_views(TestCase):
     """Check that all views respond properly"""
 
     def test_index_response(self):
-        """Check that index returns status code 200"""
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
 
     def test_about_response(self):
-        """Check that about returns status code 200"""
         response = self.client.get("/about")
         self.assertEqual(response.status_code, 200)
 
     def test_courses_response(self):
-        """Check that courses returns status code 200"""
         response = self.client.get("/courses")
         self.assertEqual(response.status_code, 200)
 
     def test_schools_response(self):
-        """Check that schools returns status code 200"""
         response = self.client.get("/schools")
         self.assertEqual(response.status_code, 200)
 
     def test_contact_response(self):
-        """Check that contact returns status code 200"""
         response = self.client.get("/contact")
         self.assertEqual(response.status_code, 200)
