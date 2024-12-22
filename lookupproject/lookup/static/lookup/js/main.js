21bd42ff3abf8d4f15969d921d49363ac5088a3b
Matthieu <martens.matthieu@hotmail.com>
2024-12-22 19:41:07 +0100

    Add a search bar to the results list

--- lookupproject/lookup/static/lookup/js/main.js
+++ lookupproject/lookup/static/lookup/js/main.js
@@ -4,14 +4,13 @@ document.addEventListener('DOMContentLoaded', function () {
     if (path === '/') {
 
         // Manage buttons to display appropriate results on index page 
-        const categoriesContainer = document.querySelector('.categories-container')
-        const resultsContainer = document.querySelector('.results-container')
         const categories = Array.from(document.querySelectorAll('.category'))
 
         categories.forEach(category => {
+            const categoryName = category.innerHTML
             category.addEventListener('click', (event) => {
                 event.preventDefault();
-                fetchCourseData(category.innerHTML, categoriesContainer, resultsContainer)
+                fetchCourseData(categoryName)
             })
         })
 
@@ -29,8 +28,8 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
 
-    // Fetch and display data
-    function fetchCourseData(categoryName, categoriesContainer, resultsContainer) {
+    // Fetch data and call function to display data
+    function fetchCourseData(categoryName) {
         fetch(`courses/get/${categoryName}`)
         .then(response => {
             if (!response.ok) {
@@ -38,22 +37,46 @@ document.addEventListener('DOMContentLoaded', function () {
             }
             return response.json();
         })
-        .then(data => renderResults(data, categoriesContainer, resultsContainer))
+        .then(data => renderResults(data))
         .catch(error => {
             console.log('error', error)
         })
     }
 
-    // Render data from lists
-    function renderResults(data, parentElement, childElement) {
-        parentElement.classList.add('d-none');
-        childElement.innerHTML = '';
+    // Renders data and adds a search bar
+    function renderResults(data) {
+        const submitCourseContainer = document.querySelector('.submit-course-container');
+        const categoriesContainer = document.querySelector('.categories-container');
+        const containersToHide = [categoriesContainer, submitCourseContainer];
+        hideUnnecessaryContainers(containersToHide)
 
-        data.forEach((piece) => {
+        const resultsContainer = document.querySelector('.results-container')
+        resultsContainer.innerHTML = '';
+
+        const searchBar = searchBarFactory()
+        resultsContainer.appendChild(searchBar);
+
+        data.forEach((course) => {
             const newElement = document.createElement('p')
-            newElement.innerHTML = piece.name
-            childElement.append(newElement)
+            newElement.innerHTML = course.name
+            resultsContainer.append(newElement)
         })
     }
+
+    function hideUnnecessaryContainers(containers) {
+        containers.forEach(container => {
+            container.classList.add('d-none')
+        })
+    }
+
+
+    function searchBarFactory() {
+        const searchBar = document.createElement('input');
+        searchBar.type = 'text'
+        searchBar.placeholder = 'Search here'
+        searchBar.classList.add('search-bar')
+        return searchBar;
+    }
+
 
 });
