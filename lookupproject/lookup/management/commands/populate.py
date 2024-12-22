4066e92dc6cd10b1d3e56e0fc3cd8e4a09ed398f
Matthieu <martens.matthieu@hotmail.com>
2024-12-22 04:58:33 +0100

    Add more verbose feedback after populate command execution

--- lookupproject/lookup/management/commands/populate.py
+++ lookupproject/lookup/management/commands/populate.py
@@ -2,18 +2,27 @@ from django.core.management.base import BaseCommand
 from ...tests.factories import SchoolFactory, CourseFactory, TeacherFactory
 
 
+# Pour une raison qui me dépasse, les données dans la boucle ne sont pas recalculées.
+# Les factories ne sont pas réexécutées après la première exécution.
 class Command(BaseCommand):
     help = 'Populate the database with sample data'
 
     def handle(self, *args, **kwargs):
-        school = SchoolFactory()
-        teacher = TeacherFactory()
-        course = CourseFactory(place=school)
+        NUMBER_OF_SAMPLES = 10
+        SAMPLED_DATA = ['school', 'teacher', 'course']
 
-        teacher.schools.add(school)
-        course.teachers.add(teacher)
-        course.save()
+        for _ in range(NUMBER_OF_SAMPLES + 1):
+            school = SchoolFactory()
+            teacher = TeacherFactory()
+            course = CourseFactory(place=school)
+
+            teacher.schools.add(school)
+            course.teachers.add(teacher)
+
+            course.save()
+            teacher.save()
+            school.save()
 
         self.stdout.write(self.style.SUCCESS(
-            'Database populated with sample data.'
+            f'Database populated with {NUMBER_OF_SAMPLES} of each {", ".join(SAMPLED_DATA)}'
             ))
