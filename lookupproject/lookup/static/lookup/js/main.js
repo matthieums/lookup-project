41df8cfea8bce54b2b4050ca380048e50452c182
Matthieu <martens.matthieu@hotmail.com>
2025-01-06 03:01:34 +0100

    refactor fetch of nearby school as a separate function

--- lookupproject/lookup/static/lookup/js/main.js
+++ lookupproject/lookup/static/lookup/js/main.js
@@ -27,43 +27,7 @@ document.addEventListener('DOMContentLoaded', function () {
         const userCoordinates = [50.8503, 4.3517];
         const radius_in_meters = 10000000000000000000
 
-        fetch('/geoschool', {
-            method: 'POST',
-            headers: { 
-                'Content-Type': 'application/json',
-                'X-CSRFToken': getCookie('csrftoken') },
-            body: JSON.stringify({ 
-                user_lat: userCoordinates[0],
-                user_lon: userCoordinates[1],
-                radius: radius_in_meters
-            })
-        })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('NETWORK RESPONSE WAS NOT OK')
-            }
-            return response.json();
-        })
-        .then(data => {
-            const nearbySchoolsContainer = document.querySelector('.nearby-schools-container');
-            nearbySchoolsContainer.innerHTML = '';
-
-            if (data.error) {
-                nearbySchoolsContainer.innerHTML = `${data.error}`;
-            } else {
-                data.forEach(school => {
-                    nearbySchoolsContainer.innerHTML += school.name;
-                });
-            }
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            document.querySelector('.nearby-schools-container').innerHTML = 'AN ERROR HAS OCCURED';
-        });
-
-
-
-
+        fetchAndDisplayNearbySchools(userCoordinates, radius_in_meters)
 
         // Manage buttons to display appropriate results on index page 
         const categories = Array.from(document.querySelectorAll('.category'))
@@ -131,9 +95,10 @@ document.addEventListener('DOMContentLoaded', function () {
         let containersToHide = [];
         
         if (path === indexView) {
+            const nearbySchoolsContainer = document.querySelector('.nearby-schools-container')
             const submitCourseContainer = document.querySelector('.submit-course-container');
             const categoriesContainer = document.querySelector('.categories-container');    
-            containersToHide.push(categoriesContainer, submitCourseContainer);
+            containersToHide.push(categoriesContainer, submitCourseContainer, nearbySchoolsContainer);
         } else if (path === teachersView) {
             const teacherContainer = document.querySelector('.teacher-container')
             containersToHide.push(teacherContainer)
@@ -256,6 +221,61 @@ document.addEventListener('DOMContentLoaded', function () {
         return card
     }
 
+
+
+    // Function to get CSRF token (required for Django's CSRF protection)
+    function getCookie(name) {
+        let cookieValue = null;
+        if (document.cookie && document.cookie !== '') {
+            const cookies = document.cookie.split(';');
+            for (let i = 0; i < cookies.length; i++) {
+                const cookie = cookies[i].trim();
+                if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
+
+    async function fetchAndDisplayNearbySchools(coordinates, radius) {
+        fetch('/geoschool', {
+            method: 'POST',
+            headers: { 
+                'Content-Type': 'application/json',
+                'X-CSRFToken': getCookie('csrftoken') },
+            body: JSON.stringify({ 
+                user_lat: coordinates[0],
+                user_lon: coordinates[1],
+                radius: radius
+            })
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('NETWORK RESPONSE WAS NOT OK')
+            }
+            return response.json();
+        })
+        .then(data => {
+            const nearbySchoolsContainer = document.querySelector('.nearby-schools-container');
+            nearbySchoolsContainer.innerHTML = '';
+    
+            if (data.error) {
+                nearbySchoolsContainer.innerHTML = `${data.error}`;
+            } else {
+                data.forEach(school => {
+                    nearbySchoolsContainer.innerHTML += school.name;
+                });
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            document.querySelector('.nearby-schools-container').innerHTML = 'AN ERROR HAS OCCURED';
+        });
+    
+    }
+   
     function getUserCoordinates() {
         return new Promise((resolve, reject) => {
             if (navigator.geolocation) {
@@ -273,19 +293,4 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 
-    // Function to get CSRF token (required for Django's CSRF protection)
-    function getCookie(name) {
-        let cookieValue = null;
-        if (document.cookie && document.cookie !== '') {
-            const cookies = document.cookie.split(';');
-            for (let i = 0; i < cookies.length; i++) {
-                const cookie = cookies[i].trim();
-                if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                    break;
-                }
-            }
-        }
-        return cookieValue;
-    }
 });
